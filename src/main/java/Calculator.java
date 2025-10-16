89146f1ff3e900068e55d2fed2e462d355e9903a
Luz Ocon <luzhdez711@gmail.com>
2025-10-16 10:54:19 -0600

    solved task

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,14 @@
 public class Calculator {
 
+    public int sum(int x, int y){
+        return x + y;
+    }
+
+    public int subtract(int x, int y){
+        return x-y;
+    }
+
+    public double divide(int x, int y){
+        return x/y;
+    }
 }
